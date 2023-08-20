0a9b099c19529b2faae3407b259020956e979024
John Caswell <shalindria@rocketmail.com>
2023-08-20 22:30:18 +0100

    Corrected typo.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import { Nunito_Sans } from 'next/font/google';
 import Nav from '@/components/Nav/Nav.jsx';
-import Theme from './theme';
+import Theme from './Theme';
 
 const nunito = Nunito_Sans({
   subsets: ['latin'],
